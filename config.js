781d01f718fbab5a31aceb273eeadbff14c081ca
yansupport1 <yansupport1@gmail.com>
2026-09-14 14:36:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -32,14 +32,14 @@ const AI_CONFIG = {
   // -------------------------------------------------
   FIREBASE_CONFIG: {
     apiKey: "AIzaSyAirO8vLQA6Z_JMDHFYndkLxgrsh84NrGk",
-    authDomain: "my-ai-f857a.firebaseapp.com",
-    databaseURL: "https://my-ai-f857a-default-rtdb.firebaseio.com/",
-    projectId: "my-ai-f857a",
-    storageBucket: "my-ai-f857a.firebasestorage.app",
-    messagingSenderId: "258485806014",
-    appId: "1:258485806014:web:33f336e6c1def52e42cfef",
-    measurementId: "G-PF3HQVEKX6",
-  },
+  authDomain: "my-ai-f857a.firebaseapp.com",
+  databaseURL: "https://my-ai-f857a-default-rtdb.firebaseio.com",
+  projectId: "my-ai-f857a",
+  storageBucket: "my-ai-f857a.firebasestorage.app",
+  messagingSenderId: "258485806014",
+  appId: "1:258485806014:web:33f336e6c1def52e42cfef",
+  measurementId: "G-PF3HQVEKX6"
+  }
 
   // -------------------------------------------------
   // API KEY per provider AI. Taruh key kamu di sini.
